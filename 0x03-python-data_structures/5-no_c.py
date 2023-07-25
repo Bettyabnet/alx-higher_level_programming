5b2729844460e746149fc956fa688a3e32dd5da8
Bettyabnet <betelhemabnet1@gmail.com>
2023-07-25 10:14:58 +0300

    err

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    str = ""
+    new_string = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-            str = str + i
-    return str
+            new_string = new_string + i
+    return new_string
