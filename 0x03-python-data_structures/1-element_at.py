10ad69a50ba05bb25303e164c8ca406bfa2e312e
Bettyabnet <betelhemabnet1@gmail.com>
2023-07-25 09:22:28 +0300

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+def element_at(my_list, idx):
+
+    for element in my_list:
+        if idx < 0:
+            return none
+        elif idx > 4:
+            return none
+        else:
+            return (idx + 1)
