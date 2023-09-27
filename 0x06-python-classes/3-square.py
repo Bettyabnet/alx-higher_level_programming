105876bf32bb04e16fc94d47282653f2f28d45c4
Bettyabnet <betelhemabnet1@gmail.com>
2023-09-28 02:21:44 +0300

    square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square that defines a square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """initialize a new square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
