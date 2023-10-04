08c80e95e0b60898b531fb400fce7180f47ae1b4
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-04 08:55:18 +0300

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
+
+
 def add_integer(a, b=98):
     """Check if a and b are integers or floats."""
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
