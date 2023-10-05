014f023f8738683cd6e647a00f2f6a6cd6374d6e
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-05 14:34:34 +0300

    text indent

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defining  a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation_chars = [".", "?", ":"]
+    start_index = 0
+    end_index = 0
+
+    while end_index < len(text):
+        if text[end_index] in punctuation_chars:
+            print(text[start_index:end_index + 1].strip())
+            print()
+            start_index = end_index + 1
+
+        end_index += 1
+
+    if start_index < len(text):
+        print(text[start_index:].strip())
