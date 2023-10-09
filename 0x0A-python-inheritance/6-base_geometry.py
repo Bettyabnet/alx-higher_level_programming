da9704603d41fa6fe5cd10c766f823d4861835a0
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-09 23:58:46 +0300

    base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module base_geometry
+which creats a class
+"""
+
+
+class BaseGeometry:
+    """class of pulic instance"""
+
+    def area(self):
+        """Raises an Exception with the message
+        'area() is  not implemented'
+        """
+        raise Exception("area() is not implemented")
