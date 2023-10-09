58297036d5d9a8dc4d800242dec49a00ba672965
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-10 01:46:48 +0300

    what

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,12 +6,18 @@ Creates a Rectangle class
 
 class BaseGeometry:
     def area(self):
+        """Raise an exception indicating that the area()
+        method is not implemented"""
+        
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
+            """ Raise a TypeError if the value is not an integer"""
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
+            """Raise a ValueError if the value is less than or 
+            equal to zero"""
             raise ValueError(f"{name} must be greater than 0")
 
 
