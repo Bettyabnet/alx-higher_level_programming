d54b55c148d8881dc22d97669c05deeb599c746c
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-12 00:54:54 +0300

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """ 
+    Defining a function that reads a text file
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
