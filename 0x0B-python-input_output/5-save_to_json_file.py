f3417eebd8286fdab3a138e15f8d6f414aa84ad1
Bettyabnet <betelhemabnet1@gmail.com>
2023-10-12 03:55:04 +0300

    why

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,12 +3,13 @@
 """
 module for to_json_string.
 """
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """
-    Return the JSON representation
+    define a function by using JSON
     """
-    return json.dumps(my_obj)
+
+    with open(filename, mode="w") as J:
+        json.dump(my_obj, J)
