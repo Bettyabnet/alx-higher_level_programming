fd266c47c7f2ccff39332e1a90c0aca76065358a
root <root@BettysHp>
2024-02-13 21:42:03 +0300

    fold

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,20 +3,14 @@
 import requests
 
 def number_of_subscribers(subreddit):
-  """Returns the number of subscribers for a given subreddit."""
-  # Set the base URL and the custom User-Agent
-  base_url = "https://www.reddit.com/"
-  headers = {"User-Agent": "alx-student"}
-
-  # Make a GET request to the subreddit's about.json endpoint
-  response = requests.get(base_url + "r/" + subreddit + "/about.json", headers=headers)
-
-  if response.status_code == 200 and response.headers["content-type"] == "application/json; charset=UTF-8":
-    data = response.json()
-
-    if data["kind"] == "t5" and not data["data"]["over18"]:
-      return data["data"]["subscribers"]
-    else:
-      return 0
-  else:
-    return 0
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if r.status_code >= 300:
+        return 0
+    json = r.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
