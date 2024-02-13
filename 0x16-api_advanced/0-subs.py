331ff9488296b5a26b62b83572c32871a48bb861
root <root@BettysHp>
2024-02-13 22:45:06 +0300

    myagentis not workin

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
-
 #!/usr/bin/python3
+""" A function that queries the Reddit API and returns the number of subscribers.
+"""
 import requests
 
 def number_of_subscribers(subreddit):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.3666666'    }
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple'+
+            'WebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    }
     r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
         subreddit), headers=headers, allow_redirects=False)
     if r.status_code >= 300:
