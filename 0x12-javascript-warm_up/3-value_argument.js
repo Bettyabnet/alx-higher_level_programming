9af7131a0b53b20cbf0e70924f86c294d12e645f
root <root@eliana>
2024-02-27 21:33:35 +0300

    argisgood

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * a script that prints a message depending of the number of arguments passed
+ */
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
