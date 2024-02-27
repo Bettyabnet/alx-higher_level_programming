7ddfb77bc3e6a046e8497080e9afe5c532644a7a
root <root@eliana>
2024-02-27 23:57:21 +0300

    data

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+ * script that imports a dictionary of occurrences by 
+ * user id and computes a dictionary of user ids by occurrence
+ */
+const { dict } = require('./101-data');
+
+const occurrencesDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (occurrences in occurrencesDict) {
+    occurrencesDict[occurrences].push(userId);
+  } else {
+    occurrencesDict[occurrences] = [userId];
+  }
+}
+
+console.log(occurrencesDict);
